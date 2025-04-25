d75729aa01bf2bdc0712245a95f164c122ca9de9
Sergei Zybin <lesouriceau@yandex.ru>
2025-04-25 17:42:06 +0700

    Update HistoryManager.java

--- src/com/yandex/kanban/service/HistoryManager.java
+++ src/com/yandex/kanban/service/HistoryManager.java
@@ -6,8 +6,10 @@ import java.util.List;
 public interface HistoryManager {
 // как ты мне
     void add(Task task);
+
 // надоел
     void remove(int id);
+
 // cо своими 'Trailing whitespace'. [Regexp]
     List<Task> getHistory();
 }
