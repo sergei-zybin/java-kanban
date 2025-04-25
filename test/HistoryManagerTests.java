357f08dba45b044ce42a994e3d17e49a28e85e86
Sergei Zybin <lesouriceau@yandex.ru>
2025-04-25 19:51:18 +0700

    Update HistoryManagerTests.java

--- test/HistoryManagerTests.java
+++ test/HistoryManagerTests.java
@@ -3,6 +3,7 @@ package com.yandex.kanban.service;
 import com.yandex.kanban.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.List; 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HistoryManagerTests {
