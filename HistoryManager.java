a44f5c978635d48dd273fa377885ff6816f579cc
Sergei Zybin <lesouriceau@yandex.ru>
2025-04-25 17:24:23 +0700

    Update HistoryManager.java

--- HistoryManager.java
+++ HistoryManager.java
@@ -4,10 +4,7 @@ import com.yandex.kanban.model.Task;
 import java.util.List;
 
 public interface HistoryManager {
-
     void add(Task task);
-
     void remove(int id);
-
     List<Task> getHistory();
 }
